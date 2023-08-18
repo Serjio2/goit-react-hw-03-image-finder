6201f683a2c02d365e7a391b438eaee93c3c9ddc
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-08-19 01:02:02 +0300

    add api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,1 +1,10 @@
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+const key = '38070377-8dbe611b16263765ee807d393';
+const perPage = 12;
+
+export const fetchImages = async () => {
+    const response = await axios.get(`?q=cat&page=1&key=${key}&image_type=photo&orientation=horizontal&per_page=${perPage}`);
+    return response.data;
+}
